1806f531cc6344d3ec76e82453e01615c03c248a
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-03 17:28:14 +0000

    show marker and game info

--- script.js
+++ script.js
@@ -10,29 +10,42 @@ let myGameBoard = {
   c2: "",
   c3: "",
 };
-// Who's playing, show a message and start the game in false (no gaming yet)
+
 let playerTurn = "";
 let warning = "";
 let playing = false;
 
-// Reset button:
+reset();
+
+//Reset button logic:
 document.querySelector(".reset").addEventListener("click", reset);
 function reset() {
-  // CLEAN the messages:
   warning = "";
-  // CHOOSE a player (aleatorio entre 1 e 0)
   let random = Math.floor(Math.random() * 2);
-  if (random === 0) {
-    // player X plays
-    player = "x";
-  } else {
-    // player O plays
-    player = "o";
-  }
-  // TAKE the 'value' of the board -> turn to empty
+  player = random === 0 ? "x" : "o";
+
   for (let i in myGameBoard) {
     myGameBoard[i] = "";
   }
 
   playing = true;
+
+  showMarker();
+  showGameInfo();
+}
+//
+function showMarker() {
+  for (let i in myGameBoard) {
+    console.log("Item: ", i);
+    let item = document.querySelector(`div[id=${i}]`);
+    if (myGameBoard[i] !== "") {
+      item.innerHTML = myGameBoard[i];
+    } else {
+      item.innerHTML = "";
+    }
+  }
+}
+function showGameInfo() {
+  document.querySelector(".turn").innerHTML = player;
+  document.querySelector(".result").innerHTML = warning;
 }
