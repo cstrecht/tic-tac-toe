ee7edc3509d3b2ec503fc7dbf612c28be08325b0
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-03 17:01:53 +0000

    reset button features

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ let myGameBoard = {
 let playerTurn = "";
 let warning = "";
 let playing = false;
+
+// Reset button:
+document.querySelector(".reset").addEventListener("click", reset);
+function reset() {
+  // CLEAN the messages:
+  warning = "";
+  // CHOOSE a player (aleatorio entre 1 e 0)
+  let random = Math.floor(Math.random() * 2);
+  if (random === 0) {
+    // player X plays
+    player = "x";
+  } else {
+    // player O plays
+    player = "o";
+  }
+  // TAKE the 'value' of the board -> turn to empty
+  for (let i in myGameBoard) {
+    myGameBoard[i] = "";
+  }
+
+  playing = true;
+}
